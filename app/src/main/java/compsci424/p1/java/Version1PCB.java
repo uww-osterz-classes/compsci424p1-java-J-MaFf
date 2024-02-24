fd4b636255dd8034ff3c9cf5f003ed97bed4ecd9
Joey Maffiola <7maffiolajoey@gmail.com>
2024-02-24 15:06:47 -0600

    Added part and children class vars, as well as a constructor for Version1PCB class.

--- app/src/main/java/compsci424/p1/java/Version1PCB.java
+++ app/src/main/java/compsci424/p1/java/Version1PCB.java
@@ -3,10 +3,26 @@
  */
 package compsci424.p1.java;
 
+import java.util.LinkedList;
+
 /**
  * The process control block structure that is used to track a
  * process's parent and children (if any) in Version 1.
  */
 public class Version1PCB {
+    int parent; // a PCB index corresponding to the process's creator (parent)
+    LinkedList<Integer> children; // each list element contains the PCB index of one child process
+
+    /**
+     * Default constructor. Use this to allocate (if needed) and
+     * initialize the PCB array, create the PCB for process 0, and do
+     * any other initialization that is needed.
+     */
+    public Version1PCB(int parent) {
+        {
+            parent = -1;
+            children = new LinkedList<Integer>();
+        }
+    }
 
 }
